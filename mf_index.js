ea4734f79fc987971b443fb2c821b167b911e438
Ashish Madhu <ashish@labh.io>
2025-05-12 10:35:30 +0530

    updates

---
+++ mf_index.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    $.get(
+        `https://devapi.labh.io/api/mutual-funds/all/?name=regular`,
+        function (responseData) {
+            const results = responseData.results;
+            results.slice(0, 6).forEach((result) => {
+                $("#funds-list").append(`
+                    <div class="fund">
+                        <div class="header d-flex justify-content-between align-items-center">
+                            <img src="${result.image_url}" alt="mf-image">
+                            <div>${result.scheme_name}</div>
+                        </div>
+                        <div class="risk">
+                            Risk Involded: <span>${
+                                result.risk_involved
+                                    ? result.risk_involved
+                                    : "N/A"
+                            }</span>
+                        </div>
+                    </div>
+                `);
+            });
+        }
+    );
+
+    $(".categories .category").on("click", function () {
+        $(".categories .category").removeClass("active");
+        $(this).toggleClass("active");
+        const value = $(this).attr("data-value");
+        $.get(
+            `https://devapi.labh.io/api/mutual-funds/all/?name=${value}`,
+            function (responseData) {
+                $("#index-contents").addClass("d-none");
+
+                const results = responseData.results;
+                $("#funds-list2").empty();
+                results.slice(0, 6).forEach((result) => {
+                    $("#funds-list2").append(`
+                    <div class="fund">
+                        <div class="header d-flex justify-content-between align-items-center">
+                            <img src="${result.image_url}" alt="mf-image">
+                            <div>${result.scheme_name}</div>
+                        </div>
+                        <div class="risk">
+                            Risk Involded: <span>${
+                                result.risk_involved
+                                    ? result.risk_involved
+                                    : "N/A"
+                            }</span>
+                        </div>
+                    </div>
+                `);
+                });
+            }
+        );
+    });
+});
