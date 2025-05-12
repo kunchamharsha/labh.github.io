9d4c040fb470b10c6a03fb1572e0554c0fdd37d3
Ashish Madhu <ashish@labh.io>
2025-05-12 11:55:42 +0530

    updates

--- mf_index.js
+++ mf_index.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
             const results = responseData.results;
             results.slice(0, 6).forEach((result) => {
                 $("#funds-list").append(`
-                    <div class="fund">
+                    <div class="fund" onclick="window.location.href='/mutual-fund/?id=${result.id}'">
                         <div class="header d-flex justify-content-between align-items-center">
                             <img src="${result.image_url}" alt="mf-image">
                             <div>${result.scheme_name}</div>
@@ -26,6 +26,7 @@ $(document).ready(function () {
     $(".categories .category").on("click", function () {
         $(".categories .category").removeClass("active");
         $(this).toggleClass("active");
+        $(".categories-fund-list").removeClass("d-none");
         const value = $(this).attr("data-value");
         $.get(
             `https://devapi.labh.io/api/mutual-funds/all/?name=${value}`,
@@ -34,9 +35,9 @@ $(document).ready(function () {
 
                 const results = responseData.results;
                 $("#funds-list2").empty();
-                results.slice(0, 6).forEach((result) => {
+                results.forEach((result) => {
                     $("#funds-list2").append(`
-                    <div class="fund">
+                    <div class="fund" onclick="window.location.href='/mutual-fund/?id=${result.id}'">
                         <div class="header d-flex justify-content-between align-items-center">
                             <img src="${result.image_url}" alt="mf-image">
                             <div>${result.scheme_name}</div>
