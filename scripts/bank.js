09a46800869ad5e1cc11b94c77986b276be51093
Ashish Madhu <ashish@labh.io>
2025-08-22 14:19:49 +0530

    updates

--- scripts/bank.js
+++ scripts/bank.js
@@ -252,9 +252,9 @@ function submitBankForm() {
             console.error("Error:", error);
             response = error.responseJSON;
             if (response.account_number) {
-                showErrors("Invalid Bank Details", response.account_number, "Try Again");
+                showErrors("Invalid Bank Details", "The account number or IFSC code seems incorrect. Please check and try again.", "Try Again");
             } else if (response.ifsc_code) {
-                showErrors("Invalid Bank Details", response.ifsc_code, "Try Again");
+                showErrors("Invalid Bank Details", "The account number or IFSC code seems incorrect. Please check and try again.", "Try Again");
             } else if (response.non_field_errors) {
                 showErrors("Invalid Bank Details", response.non_field_errors, "Try Again");
             } else {
