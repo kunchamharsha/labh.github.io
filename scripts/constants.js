0fbc5b38d885f059d9829d20a1732988c3730561
thomaslabh <thomas@labh.io>
2026-06-15 17:09:57 +0530

    add redeem status and subscribed baskets

--- scripts/constants.js
+++ scripts/constants.js
@@ -4,7 +4,7 @@ const ASSETS_URL = "https://labh.io";
 const DEBUG = false;
 
 function getCookie(name) {
-    const value = `; ${document.cookie}`;
-    const parts = value.split(`; ${name}=`);
-    if (parts.length === 2) return parts.pop().split(";").shift();
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(";").shift();
 }
